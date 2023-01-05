eb5e937159b6dd145df1270c5a5f6c759de8c243
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2023-01-05 22:41:16 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,17 +20,17 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : NexEp
-${htjava} *✉️ Nama RL* : Satria
+${htjava} *💌 Nama* : NR025
+${htjava} *✉️ Nama RL* : Nizar
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
 ${htjava} *⏰ Tanggal lahir* : Private 🥶
 ${htjava} *🎨 Umur* : PRIVATE
 ${htjava} *🧮 Kelas* : PRIVATE
-${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP
+${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP & BIKIN APLIKASI
 ${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
-${htjava} *🗺️ Tinggal* : Jawa Barat, Bekasi
-${htjava} *❤️ Waifu* : Kaede Hitotsuba
+${htjava} *🗺️ Tinggal* : Jawa Barat, CIANJUR
+${htjava} *❤️ Pacar* : 🗿
 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
 •·––––––––––––––––––––––––––·•
